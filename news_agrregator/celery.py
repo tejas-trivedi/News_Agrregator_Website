2193a76a51ec8e591bd8528b21ed4e9b929a5b73
tejas-trivedi <500077185@stu.upes.ac.in>
2020-07-16 22:59:14 +0530

    error fixed

--- news_agrregator/celery.py
+++ news_agrregator/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsagrregator.settings')
 app = Celery('news_agrregator')
 
 # Using a string here means the worker will not have to
